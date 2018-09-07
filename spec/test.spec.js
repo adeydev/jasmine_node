6c8945bb02e0ead579e1534d801bd45a6704d22b
SEPANET\craig.adey <craig.adey@sepa.org.uk>
2018-09-07 15:10:25 +0100

    first commit

---
+++ spec/test.spec.js
@@ -0,0 +1,11 @@
+describe('Test suite one', () => {
+
+    
+  
+    it('simple test 1', () => {
+        console.log('about to run our first test');
+        
+        expect(true).toBe(true);
+    });
+  });
+  
